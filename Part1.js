468ee4ac2536f349a549aa28d63060105a71cd29
Taylor <themacnair@hotmail.com>
2022-06-01 15:05:39 -0400

    Javascript Battleship in Node.js

---
+++ Part1.js
@@ -0,0 +1,71 @@
+const rs = require('readline-sync');
+
+const initQuestion = rs.keyIn('Press any key to start.');
+
+// Variables
+const letterOptions = ['A', 'B', 'C'];
+const grid = [
+    ['A1'], ['A2'], ['A3'],
+    ['B1'], ['B2'], ['B3'],
+    ['C1'], ['C2'], ['C3'],
+];
+let prevGuesses = [];
+//Random Coordinates generator
+const randomizer = maxNum => Math.floor(Math.random() * maxNum);
+const randomLetter = () => {
+    let num = randomizer(3);
+    return letterOptions[num];
+};
+//I wanted to turn randomCoords into a function for the purpose of players wanting to start a new game, but I want the game to work through at least once FIRST
+// function randomCoords(){
+let randCoord1 = randomLetter();
+let randCoord3 = randomLetter();
+let randCoord2 = randomizer(3) + 1;
+let randCoord4 = randomizer(3) + 1;
+let enemyShip1 = [randCoord1 + randCoord2];
+let enemyShip2 = [randCoord3 + randCoord4];
+let enemyCoords = [enemyShip1, enemyShip2];
+// };
+// randomCoords();
+
+
+// Guess input / start of turn
+function guesser() {
+    let coordGuess = rs.question('Enter strike location with a letters a,b, or c, and either numbers 1,2, or 3, such as "A1" or "C3".', {
+        limit: grid.flat(1),
+        limitMessage: 'Please only enter A, B, or C with 1,2,or 3, such as "C2" or "B3". '
+    });
+    let actualCoordGuess = coordGuess.toUpperCase();
+    console.log(`You have guessed ${actualCoordGuess} to attack. `);
+    prevGuesses.push(actualCoordGuess);
+    return actualCoordGuess;
+};
+// let battleTimeout = setTimeout(battleChecker(actualCoordGuess), 1500);
+// Maybe use this timeout between guess and reveal to add a lil tension.
+
+//Hit or miss
+function battleChecker(guess, enemyCoords) {
+    if (enemyCoords.includes(guess)) {
+        console.log("You have hit");
+        let lastEnemy = enemyCoords.filter(coord => coord != guess);
+        return lastEnemy;
+    } else if(prevGuesses.includes(guess)){
+        console.log(`You've already guessed ${guess}! Miss!`);
+    } else {
+        console.log('You have missed!');console.log(enemyCoords);
+    };
+}
+
+function game(guess, enemies) {
+    if (enemies[0] !== [] && enemies[1] !== []) {
+        // guesser();
+        battleChecker(playerGuess, enemies);
+        game(playerGuess, enemies);
+    } else {
+        console.log('YOU HAVE WON THE BATTLE');
+        break;
+    };
+};
+
+let startGuess = guesser();
+game(startGuess, enemyCoords);
---
+++ practice.js
@@ -0,0 +1,98 @@
+// function minMax(arr) {
+// 	let newArr = [];
+// 	let sortedArr = arr.sort((a,b)=>a-b);
+//     newArr.push(sortedArr[0]);
+// 	newArr.push(sortedArr[sortedArr.length-1]);
+// 	return newArr;
+// }
+
+// function largestSwap(num) {
+// 	let str = num.toString();
+// 	let arr = str.split("");
+//     console.log(arr);
+// 	let rev = arr.reverse();
+// 	let joint = rev.join("");
+// 	let final = parseInt(joint);
+//     return final;
+// }
+// largestSwap(28);
+// function abbrevName(name){
+//     let initials= name.split(" ");
+//     let arr=[];
+//     let holdThis1 = [];
+//     let holdThis2 = [];
+//     holdThis1.push(...initials[0]);
+//     holdThis2.push(...initials[1]);
+//     arr.push(holdThis1[0].toUpperCase(),holdThis2[0].toUpperCase());
+//     let final = arr.join(".");
+//     return final;
+//     }
+// console.log(abbrevName("bob vance"));
+
+
+
+
+
+
+let a = ['a', 'b', 'c'];
+let q = ['a', 2];
+let t = [['a', 2], ['a', 'b', 'c'], 420];
+
+console.log(a.includes('c'));
+
+// let a = 'C1';
+// let b = ['C1', 'C1'];
+// let filtered = b.filter( word => word != a)
+// // console.log(c(a));
+// console.log(filtered);
+
+
+// console.log(b[0]);
+// console.log(b);
+
+
+
+
+
+
+/////////////////////////////////////////////
+/* 
+function game(guess, enemy1=null, enemy2=null){
+coordGuess;//and conslog
+function battleChecker(guess, enemyCoords) {
+    if (enemyCoords.includes(guess)){
+    console.log("You have hit");
+    enemyCoords.delete(guess);
+      } else {
+          console.log('You have missed!');
+      };
+  }
+  if(enemy1!=null&&enemy2!==null){
+      game(guess,enemyCoords);
+  } else {
+      console.log('YOU HAVE WON THE BATTLE')
+  }
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
